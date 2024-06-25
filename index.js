57f1396170e87304e6dcd5aac605d45b0e51f865
unknown <shirisha.singh36@gmail.com>
2024-06-25 09:18:28 +0545

    first

---
+++ index.js
@@ -0,0 +1,63 @@
+// Variables
+//var
+// var x = 90;
+// console.log(x);
+// var x= 100;
+// console.log(x);
+
+// let
+// let y= 600;
+// console.log(y);
+
+// y= 100; //value can be update but variable cannot be redeclared
+// console.log(y);
+
+//const
+// const result = 100;
+// console.log(result);
+
+// result= 200; //cannot be redeclared nor update
+// console.log(result);
+
+// datatype in javascript
+
+//string
+// const output = "Ram KC";
+// console.log(output);
+// console.log(typeof output);
+
+//number (include number, float)
+// const result= 90;
+// console.log(result);
+// console.log(typeof result);
+
+//float
+// const result = 20.90;
+// console.log(result);
+// console.log(typeof result);
+
+// undefined //const cannot be used for undefined
+// let x; 
+// console.log(x);
+// console.log(typeof x);
+
+// null
+// const x= null;
+// console.log(x);
+// console.log(typeof x);
+
+//boolean
+// const isTrue = true;
+// console.log(isTrue);
+// console.log(typeof isTrue);
+
+// BigInt
+// const result = 36478832836n;
+// console.log(result);
+// console.log(typeof result);
+
+//Symbol
+// const output= Symbol("Hello World");
+// console.log(output);
+// console.log(typeof output);
+
