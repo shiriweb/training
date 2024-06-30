182a5e4b6d68f4380465a89cd55a731a8395afb6
unknown <shirisha.singh36@gmail.com>
2024-06-30 07:52:53 +0545

    data types

--- index.js
+++ index.js
@@ -31,7 +31,7 @@
 // console.log(result);
 // console.log(typeof result);
 
-//float
+// float
 // const result = 20.90;
 // console.log(result);
 // console.log(typeof result);
@@ -60,4 +60,168 @@
 // const output= Symbol("Hello World");
 // console.log(output);
 // console.log(typeof output);
+
+
+// type conversion
+// Conversion of string to Number
+// const result = "123";
+// const output = Number(result);
+// console.log(output);
+// console.log(typeof output);
+
+// const result = "123";
+// const output = parseInt(result); 
+// console.log(output);
+// console.log(typeof output);
+
+// Conversion of string to float
+// const result = "12.773";
+// const output = parseFloat(result); 
+// console.log(output);
+// console.log(typeof output);
+
+//Conversion to string
+// const result = 123;
+// const output = String(result); 
+// console.log(output);
+// console.log(typeof output);
+
+
+// const result = 123;
+// const output = result.toString(); 
+// console.log(output);
+// console.log(typeof output);
+
+//bolean into string
+// const isMarried = false;
+// const ans = String(isMarried);
+// console.log(ans);
+// console.log(typeof ans);
+
+// Object
+// const obj ={
+//     name : "Ram",
+//     age :30,
+//     gender : "Male",
+//     isMarried : false
+// };
+// console.log(obj);
+// console.log(obj.age);
+// console.log(obj["age"]);
+// console.log(typeof obj);
+
+
+//array in js
+// const array = [2,5,3,2,1];
+// console.log(array);
+// console.log(typeof array);
+
+// const date =new Date();
+// console.log(date);
+// console.log(typeof date);
+
+// emplicit
+// const result = 1 + "5";
+// console.log(result);
+// console.log(typeof result);
+
+// explicit
+// const result = 1 - "5";
+// console.log(result);
+// console.log(typeof result);
+
+// String Method
+
+// const result = "Hello World";
+// // length
+// console.log(result.length);
+
+// // charAt
+// console.log(result.charAt(2));
+
+// // indexOf
+// console.log(result.indexOf("o"));
+
+// // lastIndexOf
+// console.log(result.lastIndexOf("o"));
+
+// // lowerCase
+// console.log(result.toLowerCase());
+
+// // upperCase
+// console.log(result.toUpperCase());
+
+// // replace
+// console.log(result.replace("World", "Everyone"));
+
+// // includes
+// console.log(result.includes("World"));
+
+// // slice
+// console.log(result.slice(1,4));
+
+// //concat
+// console.log(result.concat("I love JS"));
+
+// // trim
+// let output = "                Hello World";
+// console.log(output.trim());
+
+// // split
+// console.log(result.split(" "));
+
+// concatination
+// let x ="Hello World";
+// let y = "I love JS";
+// console.log(x + " " + y);
+
+// const a = 34;
+// const b = 23;
+// result = a + b;
+// console.log("The sum of two number is : " + result);
+// console.log(`The sum of two number is : ${result}`);
+
+// operators in js
+// let y = 90;
+// y = y+ 45;
+// console.log(y);
+
+// let x = 20;
+// x += 45;
+// console.log(x);
+
+//  comparison operator
+// let a = 100;
+// let b = 45;
+// let c = "23";
+// console.log(a == c);
+// console.log(a === c);
+// console.log(a != b);
+// console.log(a != c);
+// console.log(a > b);
+// console.log(a < b);
+
+// important
+// comaprision
+// logical
+// conditional operator 
+// teranry operator
+// let ageGroup = 18;
+// console.log(ageGroup >= 18 ? "You can vote": "You cannot vote" );
+
+// unary operator
+// let x = "123";
+// let y = +x //positive
+// console.log(y);
+// console.log(typeof y);
+
+
+// const obj = {
+//     name : " Ram ",
+//     age : 23
+// };
+
+// // delete is an unary operator
+// delete obj.name;
+// console.log(obj);
 
